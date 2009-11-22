ee11e832cf13bfa9dd6e1e29e8ee0b1bd3c3d83a
Andrea Crotti <kerny404@gmail.com>
2009-11-22 22:29:19 +0100

    other triangle

--- prob_12.py
+++ prob_12.py
@@ -12,10 +12,18 @@ def triangle():
         yield n
         i += 1
         n += i
+        
+# same thing using gauss formula
+def triangle2():
+    n = 1
+    while True:
+        yield (n * (n+1) / 2)
+        n += 1
 
-t = triangle()
-while True:
-    for num in t:
+# The performances are almost identical for both generators
+def stupid(gen):
+    for num in gen:
         if divisors(num) > 500:
             print num
             break
+
