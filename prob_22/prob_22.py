e0b93ba44018725054d00d81aaf504bcecda1604
Andrea Crotti <kerny404@gmail.com>
2009-11-28 13:26:32 +0100

    imap

--- prob_22/prob_22.py
+++ prob_22/prob_22.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env/python
+from itertools import imap
 
 # Very subtle use of EVAL ;)
 # x = eval( '[' + open( 'names_prob22.txt' ).readlines()[ 0 ] + ']' )
 
 fname = "names_prob22.txt"
-names = open(fname).read()
-names = map(lambda x: x.replace('"', ''), names.split(','))
+names = imap(lambda x: x.replace('"', ''), open(fname).read().split(','))
 
 # sort them and enumerate
 enum = enumerate(sorted(names))
-def val(char): return (ord(char) - ord('A') + 1)
-def name_val(name): return sum([val(c) for c in name])
+def val(char):
+    return (ord(char) - ord('A') + 1)
+
+def name_val(name):
+    return sum([val(c) for c in name])
 
 print sum([ (a[0]+1) * name_val(a[1]) for a in enum ])
