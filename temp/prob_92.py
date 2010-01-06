74d9fc5157dc6a9357940827f418a0b14c3203f1
Andrea Crotti <kerny404@gmail.com>
2010-01-06 18:23:12 +0100

    prob 92 too slow

---
+++ temp/prob_92.py
@@ -0,0 +1,28 @@
+def prob_92(lim = 10 * 1000 * 1000):
+    tot = 0
+    # checking over a set is much faster
+    set1 = set([])
+    set89 = set([])
+    for x in xrange(1, lim):
+        print "checking %d" % x
+        val = x
+        chain = [val]
+        while True:
+ #           print "chain = %s" % str(chain)
+            # the cheapest operation goes first
+            if val == 1 or val in set1:
+               set1 = set1.union(chain)
+               print "stopping after %d steps" % len(chain)
+               break
+
+            if val == 89 or val in set89:
+                set89 = set89.union(chain)
+                print "stopping after %d steps" % len(chain)
+                tot += 1
+                break
+
+            val = sum(int(x)**2 for x in str(val))
+            chain.append(val)
+
+    #print set89
+    print tot
