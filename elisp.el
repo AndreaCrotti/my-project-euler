8be595df760bc57403a5d762cfa20fd8ca258d27
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-03-25 11:34:11 +0100

    change function to avoid conflicting names in the burden of elisp global dynamic scoping

--- elisp.el
+++ elisp.el
@@ -1,8 +1,8 @@
-(defvar temp-dir "temp/")
-(defvar solved-dir "prob_%s/")
-(defvar prob-file "prob_%s.%s")
+(defvar euler-temp-dir "temp/")
+(defvar euler-solved-dir "prob_%s/")
+(defvar euler-prob-file "prob_%s.%s")
 
-(defun check-problem (id ext)
+(defun euler-check-problem (id ext)
   (interactive)
   (setq ext (downcase ext))
   (cond
@@ -14,29 +14,29 @@
     ;; Otherwise create a link for a new file
     (concat "[[elisp:(find-file \"" (temp-problem id ext) "\")][_]]"))))
 
-(defun create-new (msg fname)
+(defun euler-create-new (msg fname)
   "creates a new file inserting the text as comment"
   (progn 
     (find-file fname)
     ;; TODO: adding automatically commenting
     (insert msg)))
 
-(defun temp-problem (id ext)
+(defun euler-temp-problem (id ext)
   (concat temp-dir
           (format prob-file id ext)))
 
-(defun solved-problem (id ext)
+(defun euler-solved-problem (id ext)
   (concat 
-   (format solved-dir id)
-   (format prob-file id ext)))
+   (format euler-solved-dir id)
+   (format euler-prob-file id ext)))
 
-(defun done ()
+(defun euler-done ()
   "This function move the file when it's working"
   (interactive)
   (if (not buffer-file-name)
       (message "your file is not saved, save it before")
     (let*
-        ((dir (concat "../" (sans-extension (file-name-nondirectory buffer-file-name))))
+        ((dir (concat "../" (euler-sans-extension (file-name-nondirectory buffer-file-name))))
          (newfile (concat dir "/" (file-name-nondirectory buffer-file-name))))
       (if
           (yes-or-no-p "sure you solved the problem?")
@@ -49,5 +49,5 @@
             (kill-buffer))
         (message "yes check better your results first")))))
 
-(defun sans-extension (fname)
+(defun euler-sans-extension (fname)
   (substring fname 0 (string-match "\\." fname)))
