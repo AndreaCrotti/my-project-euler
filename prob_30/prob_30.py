28dfb1bdce170fcc8b195566ca9754b01362b0da
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-07-07 19:32:20 +0200

    refactoring without reduce

--- prob_30/prob_30.py
+++ prob_30/prob_30.py
@@ -2,9 +2,11 @@
 # -*- encoding: utf-8 -*-
 
 # The maximum number must be 5 * 9^5
-bound = 5 * 9**5
+BOUND = 5 * 9**5
 
-def toPower(n):
-    return sum(map(lambda x: int(x)**5, str(n)))
+def to_power(n):
+    "Return the sum of the 5th power of each digit"
+    digits = (int(x) for x in str(n))
+    return sum(x**5 for x in digits)
 
-print sum(filter(lambda x: toPower(x) == x,  xrange(2, bound)))
+print(sum(x for x in range(2, BOUND) if to_power(x) == x))
